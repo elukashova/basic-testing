b2ec58a22c1ef4bfce12a0983c577c6e6ef54284
elukashova <ele.lukashova@gmail.com>
2024-02-08 23:38:09 +0100

    feat: implement table-tests solution

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,163 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  {
+    message: 'should add two numbers',
+    a: 1,
+    b: 2,
+    action: Action.Add,
+    expected: 3,
+  },
+  {
+    message: 'should add two numbers',
+    a: 2,
+    b: 2,
+    action: Action.Add,
+    expected: 4,
+  },
+  {
+    message: 'should add two numbers',
+    a: 3,
+    b: 2,
+    action: Action.Add,
+    expected: 5,
+  },
+
+  {
+    message: 'should subtract two numbers',
+    a: 1,
+    b: 2,
+    action: Action.Subtract,
+    expected: -1,
+  },
+  {
+    message: 'should subtract two numbers',
+    a: 2,
+    b: 2,
+    action: Action.Subtract,
+    expected: 0,
+  },
+  {
+    message: 'should subtract two numbers',
+    a: 3,
+    b: 2,
+    action: Action.Subtract,
+    expected: 1,
+  },
+
+  {
+    message: 'should multiply two numbers',
+    a: 1,
+    b: 2,
+    action: Action.Multiply,
+    expected: 2,
+  },
+  {
+    message: 'should multiply two numbers',
+    a: 2,
+    b: 2,
+    action: Action.Multiply,
+    expected: 4,
+  },
+  {
+    message: 'should multiply two numbers',
+    a: 3,
+    b: 2,
+    action: Action.Multiply,
+    expected: 6,
+  },
+
+  {
+    message: 'should divide two numbers',
+    a: 8,
+    b: 2,
+    action: Action.Divide,
+    expected: 4,
+  },
+  {
+    message: 'should divide two numbers',
+    a: 24,
+    b: 2,
+    action: Action.Divide,
+    expected: 12,
+  },
+  {
+    message: 'should divide two numbers',
+    a: 9,
+    b: 3,
+    action: Action.Divide,
+    expected: 3,
+  },
+
+  {
+    message: 'should exponentiate two numbers',
+    a: 1,
+    b: 1,
+    action: Action.Exponentiate,
+    expected: 1,
+  },
+  {
+    message: 'should exponentiate two numbers',
+    a: 4,
+    b: 4,
+    action: Action.Exponentiate,
+    expected: 256,
+  },
+  {
+    message: 'should exponentiate two numbers',
+    a: 6,
+    b: 6,
+    action: Action.Exponentiate,
+    expected: 46656,
+  },
+
+  {
+    message: 'should return null for invalid action',
+    a: 1,
+    b: 1,
+    action: 'action',
+    expected: null,
+  },
+  {
+    message: 'should return null for invalid action',
+    a: 4,
+    b: 4,
+    action: 'action',
+    expected: null,
+  },
+  {
+    message: 'should return null for invalid action',
+    a: 6,
+    b: 6,
+    action: 'action',
+    expected: null,
+  },
+
+  {
+    message: 'should return null for invalid arguments',
+    a: 1,
+    b: '1',
+    action: Action.Multiply,
+    expected: null,
+  },
+  {
+    message: 'should return null for invalid arguments',
+    a: undefined,
+    b: 4,
+    action: Action.Exponentiate,
+    expected: null,
+  },
+  {
+    message: 'should return null for invalid arguments',
+    a: 6,
+    b: null,
+    action: Action.Divide,
+    expected: null,
+  },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  test.each(testCases)('$message', ({ expected, ...testCase }) => {
+    expect(simpleCalculator(testCase)).toBe(expected);
   });
-  // Consider to use Jest table tests API to test all cases above
 });
